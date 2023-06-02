9ce00e8563f93a49b451e9163700838b7b051263
Chris <cpaterson152@gmail.com>
2023-06-02 11:43:16 -0700

    Update displayPhotos function and fetch logic

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 // Declare variables
-let container = document.querySelector('#response-div');
-let searchBarForm = document.querySelector('#search-form');
+let container = document.querySelector("#response-div");
+let searchBarForm = document.querySelector("#search-form");
 
 //Display photos function
 function displayPhotos(images) {
-    images.forEach(image => {
-        let imgDiv = document.createElement('div');
-        imgDiv.innerHTML = `
+  images.forEach((image) => {
+    let imgDiv = document.createElement("div");
+    imgDiv.innerHTML = `
             <div class="img-div">
                <img src=${image.src.small}>
             </div>
         `;
-        container.appendChild(imgDiv);
-    })
- }
+    container.appendChild(imgDiv);
+  });
+}
 
 //Dynamic photo rendering with event listener
- searchBarForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    container.innerHTML = "";
-    let inputValue = document.querySelector('#search-input').value;
-    fetch(`https://api.pexels.com/v1/search?query=${inputValue}&per_page=20`,{
-        headers: {
-          Authorization: "LB6rfF6oyTb61KvNfA1WZMeWc3MB8eL9eVNeGadUuHlA0Z7sYclbEhi1"
-        }
-      })
-         .then(resp => {
-           return resp.json();
-         })
-         .then(data => {
-           displayPhotos(data.photos);
-         })
- });
-
-//Defaults chimpanzee photos
- fetch("https://api.pexels.com/v1/search?query=chimpanzees&per_page=20",{
-   headers: {
-     Authorization: "LB6rfF6oyTb61KvNfA1WZMeWc3MB8eL9eVNeGadUuHlA0Z7sYclbEhi1"
-   }
- })
-    .then(resp => {
+searchBarForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  container.innerHTML = "";
+  let inputValue = document.querySelector("#search-input").value;
+  fetch(`https://api.pexels.com/v1/search?query=${inputValue}&per_page=20`, {
+    headers: {
+      Authorization: "LB6rfF6oyTb61KvNfA1WZMeWc3MB8eL9eVNeGadUuHlA0Z7sYclbEhi1",
+    },
+  })
+    .then((resp) => {
       return resp.json();
     })
-    .then(data => {
+    .then((data) => {
       displayPhotos(data.photos);
-    })
+    });
+});
+
+//Defaults chimpanzee photos
+fetch("https://api.pexels.com/v1/search?query=chimpanzees&per_page=20", {
+  headers: {
+    Authorization: "LB6rfF6oyTb61KvNfA1WZMeWc3MB8eL9eVNeGadUuHlA0Z7sYclbEhi1",
+  },
+})
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    displayPhotos(data.photos);
+  });
